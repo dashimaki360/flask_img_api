795f50b71aebe61eaa4af409a7256ae4388fe7b5
Yamaguchi <17.punch@gmail.com>
2018-02-21 20:30:32 +0900

    change during

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory, session
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 from werkzeug import secure_filename
 
 import process
@@ -11,9 +11,9 @@ proc = process.processImg()
 UPLOAD_FOLDER = './uploads'
 OUTPUT_FOLDER = './outputs'
 ALLOWED_EXTENSIONS = set(['png', 'PNG', 'jpg', 'JPG', 'gif', 'GIF'])
+
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['OUTPUT_FOLDER'] = OUTPUT_FOLDER
-app.config['SECRET_KEY'] = os.urandom(24)
 
 
 def allowed_file(filename):
@@ -31,10 +31,12 @@ def send():
     if request.method == 'POST':
         img_file = request.files['img_file']
         if img_file and allowed_file(img_file.filename):
+            # TODO add timestamp to filename
             filename = secure_filename(img_file.filename)
             img_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             img_file.save(img_path)
             img_url = '/uploads/' + filename
+            base64.b64encode(img_file)
 
             out_path = os.path.join(app.config['OUTPUT_FOLDER'], filename)
             out_url = '/outputs/' + filename
@@ -44,6 +46,27 @@ def send():
             return ''' <p>許可されていない拡張子です</p> '''
     else:
         return redirect(url_for('index'))
+
+
+@app.route('/img_api', methods=['POST'])
+def imgApi():
+    # parse body json
+    request.data
+    in_img_base64 = 
+    setting = 
+
+
+    # call img process
+    out_img_base64 = proc.do(in_img_base64, setting)
+
+    # make response json
+    response = {
+
+    }
+
+    # return
+    return jsonify(response)
+
 
 
 @app.route('/uploads/<filename>')
--- process.py
+++ process.py
@@ -1,12 +1,41 @@
-from skimage import io
-from skimage.color import rgb2gray
+import base64
+import cv2
+import numpy as np
 
 
 class processImg():
     def __init__(self):
+        '''
+        process initiarize method 
+        ex) read table file, setting, initiarize
+        '''
         pass
 
-    def do(self, in_img_path, out_img_path):
-        im = io.imread(in_img_path)
-        gray = rgb2gray(im)
-        io.imsave(out_img_path, gray)
+    def readb64(self, base64_string):
+        img = base64.b64decode(base64_string)
+        npimg = np.fromstring(img, dtype=np.uint8)
+        cv_img = cv2.imdecode(npimg, 1)
+        return cv_img
+
+    def writeb64(self, cv_img):
+        retval, buffer = cv2.imencode('.jpg', cv_img)
+        base64_img = base64.b64encode(buffer)
+        return base64_img
+
+    def do(self, in_img_base64, setting):
+        # sample
+        in_img_cv = self.read64(in_img_base64)
+        out_img_cv, result = self.yourMethod(in_img_cv, setting)
+        out_img_base64 = self.write64(out_img_cv)
+        return out_img_base64, result
+
+    def yourMethod(self, in_img_cv, setting):
+        '''
+        please write thid method
+        as you like
+        '''
+        h, w = in_img_cv.shape[0, 1]
+        gray_img_cv = cv2.cvtColor(in_img_cv, cv2.COLOR_BGR2GRAY)
+        result = {'color': 'gray',
+                  'size': [w, h]}
+        return gray_img_cv, result
